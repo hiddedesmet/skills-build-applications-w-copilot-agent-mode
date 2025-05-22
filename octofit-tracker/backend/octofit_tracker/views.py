ebf3f1d7b976bc4975ecf878324505eac25d6c48
Hidde de Smet <68814600+hiddedesmet@users.noreply.github.com>
2025-05-22 09:20:54 +0000

    Update base URL in api_root function to point to the production server

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,7 +6,7 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = 'http://localhost:8000/'
+    base_url = 'https://turbo-eureka-wxxgjqq5jq6f9vxx.github.dev-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/',
         'teams': base_url + 'api/teams/',
